38d6c09ee332518d1aae0fc1b485d2712f2dc053
Vibe <vibexie@gmail.com>
2022-08-31 14:47:51 +0800

    优化isGpsOpened判断逻辑
    处理某些机型如小米11，LocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)为false的问题

--- android/src/main/java/com/reactnative/platform/GpsSettings.java
+++ android/src/main/java/com/reactnative/platform/GpsSettings.java
@@ -3,6 +3,7 @@ package com.reactnative.platform;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.os.Build;
 import android.provider.Settings;
 
 public class GpsSettings {
@@ -10,13 +11,21 @@ public class GpsSettings {
     }
 
     public static boolean isGpsOpened(final Context context) {
-        LocationManager locationManager
-                = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        // 通过GPS卫星定位，定位级别可以精确到街（通过24颗卫星定位，在室外和空旷的地方定位准确、速度快）
-        boolean gps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        // 通过WLAN或移动网络(3G/2G)确定的位置（也称作AGPS，辅助GPS定位。主要用于在室内或遮盖物（建筑群或茂密的深林等）密集的地方定位）
-        boolean network = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        return gps && network;
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        
+        if (locationManager == null) {
+            return false;
+        }
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            return locationManager.isLocationEnabled();
+        } else {
+            // 通过GPS卫星定位，定位级别可以精确到街（通过24颗卫星定位，在室外和空旷的地方定位准确、速度快）
+            boolean gps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            // 通过WLAN或移动网络(3G/2G)确定的位置（也称作AGPS，辅助GPS定位。主要用于在室内或遮盖物（建筑群或茂密的深林等）密集的地方定位）
+            boolean network = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            return gps && network;
+        }
     }
 
     public static void openGps(Context context) {
